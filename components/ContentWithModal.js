029245845cf717deb53cc0af422f4a968787d31c
LiChiaChiang <lichiachiang336@gmail.com>
2025-07-10 16:59:39 +0800

    add .gitignore

--- components/ContentWithModal.js
+++ components/ContentWithModal.js
@@ -303,7 +303,7 @@ export default function ContentWithModal() {
               const safeName = escapeHtml(trimmedName);
               const safeMessage = escapeHtml(trimmedMessage);
 
-              // 這裡可以送出 safeName 和 safeMessage
+              // 這裡可以在submit送出時在console看 safeName 和 safeMessage
               console.log(`Submit: safeName:${safeName}, safeMessage:${safeMessage}`);
             }}
             className="w-full px-4 py-2 bg-[#6760AB] rounded hover:bg-[#544DA1] text-sm md:text-xl"
