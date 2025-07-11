718023dfe1ece691822cc832a8c7f44143a61585
LiChiaChiang <lichiachiang336@gmail.com>
2025-07-11 12:47:13 +0800

    add framer-motion in result modal

--- components/ResultModal.js
+++ components/ResultModal.js
@@ -1,5 +1,8 @@
 "use client";
 
+import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
+
 export default function ResultModal({
   isOpen,
   onClose,
@@ -7,23 +10,41 @@ export default function ResultModal({
   closeMainModal,
   closeResultModal,
 }) {
+  const router = useRouter();
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 bg-gray bg-opacity-5 backdrop-blur-md flex justify-center items-center z-50">
-      <div className="bg-[#0e0e0e] shadow-[0_0_4px_rgba(0,0,0,0.8),0_0_20px_rgba(255,255,255,0.9)]  p-6 rounded-xl text-white w-80 text-center space-y-4">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.95 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1.5, ease: "easeInOut" }}
+        className="bg-[#0e0e0e] shadow-[0_0_4px_rgba(0,0,0,0.8),0_0_20px_rgba(255,255,255,0.9)] p-6 rounded-xl text-white w-80 text-center space-y-6"
+      >
         {type === "success" ? (
           <>
-            <p className="text-lg font-bold text-[#D0A760]">
-              Message submitted successfully!
-            </p>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 2, ease: "easeInOut" }}
+              className="space-y-2"
+            >
+              <p className="text-lg font-bold text-[#D0A760]">
+                Thank you for being part of the Human Universe.
+              </p>
+              <p className="text-base">
+                {" "}
+                A new star now carries your words across the night.
+              </p>
+            </motion.div>
+
             <div className="flex justify-center gap-4">
               <button
                 className="px-4 py-2 bg-[#84B2DB] hover:bg-[#5B93C4]  rounded"
                 onClick={() => {
                   closeResultModal(); // 關掉 ResultModal
                   closeMainModal(); // 關掉留言 Modal
-                  window.location.href = "/"; // 去首頁
+                   router.push("/");// 去首頁
                 }}
               >
                 Home Page →
@@ -33,7 +54,7 @@ export default function ResultModal({
                 onClick={() => {
                   closeResultModal();
                   closeMainModal();
-                  window.location.href = "/board";
+                  router.push("/board");
                 }}
               >
                 Star Board →
@@ -42,10 +63,15 @@ export default function ResultModal({
           </>
         ) : (
           <>
-            <p className="text-lg md:text-xl font-bold text-[#D0A760]">
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1.5, ease: "easeInOut" }}
+              className="text-lg md:text-xl font-bold text-[#D0A760]"
+            >
               You’ve already submitted a message today. <br />
               Please try again tomorrow!
-            </p>
+            </motion.p>
             <button
               className="px-4 py-2 bg-[#6760AB] hover:bg-[#544DA1] text-sm md:text-xl  rounded mt-2"
               onClick={closeResultModal}
@@ -54,7 +80,7 @@ export default function ResultModal({
             </button>
           </>
         )}
-      </div>
+      </motion.div>
     </div>
   );
 }
